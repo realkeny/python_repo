eeef80678498c9d7c3bfcfb03114b9154fb32f3a
kenny.adetutu@gmail.com <KAdetutu@suffolk.com>
2019-08-23 14:14:10 -0400

    commit

---
+++ DirUsage.py
@@ -0,0 +1,40 @@
+import os, sys
+from os.path import join, getsize
+#rootDir = "C:\\Scripts"
+def getfoldersizes(rootDir):
+    dir_size = [(dir,sum([getsize(join(dir,file)) for file in fileList])) for dir, subdir, fileList in os.walk(rootDir)]
+    sorted_dir_size = sorted(dir_size, key = lambda x:x[1], reverse = True)
+    return sorted_dir_size
+
+
+
+def gettopdir(dirpath, num):
+    dir_list = getfoldersizes(dirpath)
+    return dir_list[:num]
+        
+
+def print_header(msg,border):
+    header_border = border * len(msg)
+    print(header_border)
+    print(msg)
+    print(header_border + "\n")
+    print("Directory name" + "\tSize")
+
+
+def printout(list_object):
+    print_header("Directory listing by size", "=")
+    for x,y in list_object:
+        print(x,y)
+
+
+
+def main():
+   # rootpath = "C:\Scripts"
+   if len(sys.argv) >= 2:
+       dir_size = getfoldersizes(sys.argv[1])
+       printout(dir_size)
+
+
+
+if __name__ == "__main__":
+    main()
